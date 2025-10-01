1a4e7340eaab5d16e242371312dfa59a998142b7
KJanaki-12 <kenchugundujanaki@gmail.com>
2025-10-01 08:49:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function validEmail(str) {
   //your JS code here.
+	if(str.includes(".com") || str.includes(".con.in") || str.includes(".edu")){
+		if(str.includes("@")){
+			if(str.indexOf('@') === 0){
+				return false;
+			}
+		}
+		return true;
+	}else{
+		return false;
+	}
 }
 
 // Do not change the code below.
